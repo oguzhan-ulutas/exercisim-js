96d59917e173c1a5f1b241b3f62585acbcbc906c
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-03-29 16:47:07 +0300

    Solve resistors-colors

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+]
+
+export const colorCode = (color) => {
+  
+  
+    return COLORS.indexOf(color)
+  
+  
 };
 
-export const COLORS = undefined;
+
