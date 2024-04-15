18783e5de041f5d25274cab6b6959adc05176d3c
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-15 19:16:41 +0300

    Solve gigaseconds

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+
+  // date to seconds
+  const timestamp = date.getTime()
+
+  const gs = Math.floor(timestamp+1000000000000)
+
+  const newDate = new Date(gs)
+
+  return newDate
+  
 };
