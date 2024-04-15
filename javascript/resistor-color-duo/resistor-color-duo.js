27a1c846b39688d7416bafc9ea7d0e290ad498ad
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-15 14:43:23 +0300

    Solve resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+const colorMap = {
+  black : 0,
+  brown : 1,
+  red : 2,
+  orange : 3,
+  yellow : 4,
+  green : 5,
+  blue :6 ,
+  violet : 7,
+  grey : 8,
+  white : 9
+
+}
+  
+
+
+export const decodedValue = (colorArr) => {
+  let colorCode = `${colorMap[colorArr[0]]}` + `${colorMap[colorArr[1]]}`
+  
+  
+  return parseFloat(colorCode)
 };
