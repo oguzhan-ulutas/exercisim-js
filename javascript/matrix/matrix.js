0f384c5247365a49cb9deeaf614312e601919f18
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-16 15:10:28 +0300

    Solve matrix

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -4,15 +4,26 @@
 //
 
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(matrixString) {
+    this.matrix = matrixString.split("\n").map(item=>item.split(" ").map(item=>Number(item)))
+    
+    
   }
 
   get rows() {
-    throw new Error('Remove this statement and implement this function');
+    return this.matrix
   }
 
   get columns() {
-    throw new Error('Remove this statement and implement this function');
+    const rows = this.matrix.length;
+    const cols = this.matrix[0].length;
+    const transposedMatrix = Array.from({ length: cols }, () => Array(rows));
+
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        transposedMatrix[i][j] = this.matrix[j][i];
+      }
+    }
+    return transposedMatrix;
   }
 }
