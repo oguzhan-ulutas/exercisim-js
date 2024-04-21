3dd9b941ce5f71b28cfd93d0622684f5472c2741
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-21 21:05:33 +0300

    Solve pascals-triangle

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this statement and implement this function');
+export const rows = (numRows) => {
+  const pascalTriangle = [];
+
+  for (let i = 0; i < numRows; i++) {
+    const newRow = [];
+
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        newRow.push(1);
+      } else {
+        const newElement = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+        newRow.push(newElement);
+      }
+    }
+    pascalTriangle.push(newRow);
+  }
+
+  return pascalTriangle;
 };
