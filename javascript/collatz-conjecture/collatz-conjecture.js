dd5a82ccd4679a30b363914060c51d00ab1d1415
oguzhan-ulutas <mkoulutas@gmail.com>
2024-07-16 12:11:27 +0300

    Solve collatz conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+export const steps = (number) => {
+  console.log(Boolean(-15))
+  if (parseInt(number) < 1) throw new Error('Only positive numbers are allowed');
+
+  let steps = 0
+
+  while (number !== 1 && steps < 200) {
+    if (number%2 === 0){
+      number = number / 2
+    }else {
+      number = (3*number) + 1
+    }
+
+    steps++
+  }
+
+  return steps
 };
